c7657558a4897e74fc64e430f4dcb4a91d19c2e8
Darick33 <sebolin2004@gmail.com>
2024-08-05 21:41:47 -0500

    fix: delete file error

--- src/main/java/com/appgym/infra/error/DatosRespuestaError.java
+++
@@ -1,6 +0,0 @@
-package com.appgym.infra.error;
-
-public record DatosRespuestaError(
-        String error
-) {
-}
--- src/main/java/com/appgym/infra/error/ValidacionIntegridad.java
+++
@@ -1,8 +0,0 @@
-package com.appgym.infra.error;
-
-public class ValidacionIntegridad extends RuntimeException {
-    public ValidacionIntegridad(String s) {
-        super(s);
-    }
-
-}
